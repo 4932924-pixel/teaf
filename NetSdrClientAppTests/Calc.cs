d501cc38dc1632c68d45c052b190e05698eb3f6b
4932924-pixel <4932924@stud.kai.edu.ua>
2026-01-14 23:12:50 +0200

    Create Calc.cs

---
+++ NetSdrClientAppTests/Calc.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using NetSdrClientApp; 
+
+namespace NetSdrClientAppTests
+{
+    public class SimpleTests
+    {
+        [Fact]
+        public void Test_Math_Example()
+        {
+            Assert.True(true); // Тимчасова заглушка, щоб тест пройшов
+        }
+    }
+}
